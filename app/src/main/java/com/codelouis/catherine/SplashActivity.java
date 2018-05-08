b14aca87f910ecf329c00526c925e05b8c0ac17d
Louis <manson.louis@gmail.com>
2018-05-08 13:05:44 -0700

    add animation splash

--- app/src/main/java/com/codelouis/catherine/SplashActivity.java
+++ app/src/main/java/com/codelouis/catherine/SplashActivity.java
@@ -1,13 +1,31 @@
 package com.codelouis.catherine;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private LottieAnimationView animationView;
+    private final int DURACION_SPLASH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        animationView = (LottieAnimationView) findViewById(R.id.animation_view);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
     }
 }
